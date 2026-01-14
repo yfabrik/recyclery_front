4060c63479e324fb10d6fc5105ef982e7befcad7
yfabrik <138105766+yfabrik@users.noreply.github.com>
2026-01-14 10:15:29 +0100

    format

--- src/services/api/collectionSchedules.ts
+++ src/services/api/collectionSchedules.ts
@@ -1,20 +1,19 @@
 import axiosInstance from "./axios";
 interface filters {
-  filters: {
-    active_only: string;
-  };
+  active_only: string;
 }
-export const getCollectionSchedules = ({ filters }: filters) =>
+export const getCollectionSchedules = (filters?: filters) =>
   axiosInstance.get("/api/collection-schedules", { params: filters });
 
-export const updateCollectionSchedule = (id:number, data) =>
+export const updateCollectionSchedule = (id: number, data) =>
   axiosInstance.put(`/api/collection-schedules/${id}`, data);
 export const createCollectionSchedule = (data) =>
   axiosInstance.create("/api/collection-schedules", data);
-export const deleteCollectionSchedule = (id:number) =>
+export const deleteCollectionSchedule = (id: number) =>
   axiosInstance.delete(`/api/collection-schedules/${id}`);
 
+export const getCollectionReceipt = (id: number) =>
+  axiosInstance.get(`/api/collection-schedules/${id}/receipt`);
 
-export const getCollectionReceipt =(id:number)=>axiosInstance.get(`/api/collection-schedules/${id}/receipt`)
-
-export const createCollectionReceipt = (id:number,data)=>axiosInstance.post(`/api/collection-schedules/${id}/receipt`,data)
+export const createCollectionReceipt = (id: number, data) =>
+  axiosInstance.post(`/api/collection-schedules/${id}/receipt`, data);
