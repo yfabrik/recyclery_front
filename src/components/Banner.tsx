ffbd194747b865a3d285fc5060f5df22806a4061
yfabrik <138105766+yfabrik@users.noreply.github.com>
2026-01-19 13:38:33 +0100

    bug banner

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -24,7 +24,7 @@ const Banner = () => {
       }}
     >
       <Typography variant="body2" align="left">
-        Hello World - {envText}
+        bug trouvé ? bug reporté: <a href={`mailto:${envText}`}> {envText}</a>
       </Typography>
     </Box>
   );
