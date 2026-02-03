85f0311d99f8f156efe6928cbeb1cddafd88a08c
Votre Nom <Vous@exemple.com>
2026-02-03 15:55:33 +0100

    weekday helpers

--- src/services/dateService.ts
+++ src/services/dateService.ts
@@ -13,6 +13,19 @@ export const weekDaysMap = () => {
         return prev.set(current, formatter.format(jour))
     }, new Map())
 }
+export const weekDaysArray = () => {
+    const formatter = new Intl.DateTimeFormat("fr-FR", { weekday: "long" })
+    return [1, 2, 3, 4, 5, 6, 0].map(day => {
+        const jour = new Date(1970, 0, 4 + day) // 04/01/1970=dimanche = 0
+        return { key: day, label: formatter.format(jour) }
+    })
+}
+
+export const getDay = (n: number) => {
+    const formatter = new Intl.DateTimeFormat("fr-FR", { weekday: "long" })
+    const jour = new Date(1970, 0, 4 + n)
+    return formatter.format(jour)
+}
 
 export const formatDate = (d: Date) => {
     return d.toLocaleDateString("fr-FR")
